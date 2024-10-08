b452cefc670fdba4b54fffe094d917f1f144356c
Francisco-Rocco-Allegri <francisco.rocco-allegri@mths.ca>
2024-10-08 10:48:37 -0400

    changed comment and readme file

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Francisco
+ * Created on: Sep 2024
+ * This program lights up  3 neopixels
 */
 
 basic.showString('Hello, World!')
